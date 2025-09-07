2309c032d81594582ad311ab03e785ddcfeee90a
Chaiwat S <iwatth@gmail.com>
2025-09-07 11:49:18 -0700

    feat: sub-command tree

--- main.go
+++ main.go
@@ -24,6 +24,34 @@ type Certificate struct {
 }
 
 func main() {
+	if len(os.Args) < 2 {
+		printUsage()
+		os.Exit(1)
+	}
+
+	command := os.Args[1]
+
+	switch command {
+	case "tree":
+		runTreeCommand()
+	case "help", "-h", "--help":
+		printUsage()
+	default:
+		fmt.Printf("Unknown command: %s\n\n", command)
+		printUsage()
+		os.Exit(1)
+	}
+}
+
+func printUsage() {
+	fmt.Println("Usage: vibecert <command> [arguments]")
+	fmt.Println("")
+	fmt.Println("Available commands:")
+	fmt.Println("  tree    Display certificate dependency tree")
+	fmt.Println("  help    Show this help message")
+}
+
+func runTreeCommand() {
 	certs, err := loadCertificates("data/certs")
 	if err != nil {
 		log.Fatal("Error loading certificates:", err)
